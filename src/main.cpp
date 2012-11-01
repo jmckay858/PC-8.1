ee6284c3d901c23ca8bb211d128eaa602578bb10
jmckay858 <sdstat@gmail.com>
2012-11-01 16:39:24 -0700

    Re done to include a binary search for project 8.4

--- src/main.cpp
+++ src/main.cpp
@@ -11,40 +11,82 @@
 
 #include <iostream>
 using namespace std;
-bool searchCardNumbers(const int cards[], int SIZE, int cardNumber);
+
+int searchCardNumbers(const int cards[], int SIZE, int cardNumber);
+void sortCards(int cards[], int SIZE);
+
 const int SIZE = 18;
 
 int main() {
-	int cardNumber;
+	int cardNumber, result;
 	int cards[SIZE] = {5658845, 4520125, 7895122, 8777541, 8451277, 1302850,
 					   8080152, 4562555, 5552012, 5050552, 7825877, 1250255,
 					   1005231, 6545231, 3852085, 7576651, 7881200, 4581002};
 	cout << "\n   "
 		 << "What is the account number: ";
 	cin >> cardNumber;
-	if(searchCardNumbers(cards, SIZE, cardNumber))
+	sortCards(cards, SIZE);
+	result = searchCardNumbers(cards, SIZE, cardNumber);
+	if(result == -1)
+		{
+			cout << "   "
+				 << "The account number is invalid." << endl;
+
+		}
+	else
 		{
 			cout << "   "
 				 << "The account number is valid." << endl;
-
-		} else {
-			cout << "   "
-				 << "The account number is invalid." << endl;
 		}
 	return 0;
 }
 
-bool searchCardNumbers(const int cards[], int SIZE, int cardNumber)
+int searchCardNumbers(const int cards[], int SIZE, int cardNumber)
 {
-	int index = 0;
+	int first = 0,
+		last = SIZE -1,
+		middle,
+		position = -1;
 
-	while (index < SIZE)
+	bool found = false;
+
+	while (!found && first <= last)
 	{
-		if (cards[index] == cardNumber)
+		middle = first + last / 2;
+		if (cards[middle] == cardNumber)
 		{
-			return true;
+			found = true;
+			position = middle;
 		}
-		index++;
+		else if (cards[middle] > cardNumber)
+			last = middle - 1;
+		else
+			first = middle + 1;
 	}
-	return false;
+	return position;
 }
+
+void sortCards(int cards[], int SIZE)
+{
+	int startScan, minIndex, minValue;
+
+	for (startScan = 0; startScan < (SIZE - 1); startScan++)
+	{
+		minIndex = startScan;
+		minValue = cards[startScan];
+		for(int index = startScan + 1; index < SIZE; index++)
+		{
+			if (cards[index] < minValue)
+			{
+				minValue = cards[index];
+				minIndex = index;
+			}
+		}
+		cards[minIndex] = cards[startScan];
+		cards[startScan] = minValue;
+	}
+}
+
+
+
+
